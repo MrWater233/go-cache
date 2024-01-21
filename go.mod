ae993ce31aec99ddb90b4ef5bafc7257302dc7ac
Keract <wanjingmiao@gmail.com>
2024-01-21 11:41:24 +0800

    feat/lru

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MrWater233/go-cache
+
+go 1.21
---
+++ lru/lru.go
@@ -0,0 +1,74 @@
+package lru
+
+import "container/list"
+
+// 双向链表存储的数据类型
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+type Cache struct {
+	maxBytes  int64                         // 最大使用的内存
+	nbytes    int64                         // 已使用内存
+	ll        *list.List                    // 双向链表
+	cache     map[string]*list.Element      // dict，val指向双向链表元素
+	OnEvicted func(key string, value Value) // 元素被删除时的回调函数
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToBack(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+
+	return
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key: key, value: value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
